b07e50d6a0dd4ffc8f1ed60b45da275c246dc444
chenguanzhou <chenguanzhou123@163.com>
2015-08-28 22:05:19 +0800

    localization

--- SQLiteManager/App.xaml.cs
+++ SQLiteManager/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +14,9 @@ namespace SQLiteManager
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
+        }
     }
 }
